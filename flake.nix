198b79882b231c3b7ca7b87ab4d784eed5df41cc
Howard-12 <s0989175085@gmail.com>
2025-06-05 17:17:36 +1000

    zig template

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "flake zig dev";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.zig_overlay.url = "github:mitchellh/zig-overlay";
+  inputs.zig_overlay.inputs.flake-utils.follows = "flake-utils";
+
+  inputs.zls.url = "github:zigtools/zls";
+
+  outputs = input@{ self, nixpkgs, flake-utils, zig_overlay, zls, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+
+        # compiler version
+        zig_version = "0.14.1";
+        zig = zig_overlay.packages.${system}.${zig_version};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            zls.packages.${system}.default
+            zig
+          ];
+
+          shellHook = ''
+            echo 'zig' "$(zig version)"
+          '';
+        };
+      }
+    );
+}
