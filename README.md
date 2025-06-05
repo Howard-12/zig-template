db457a621175c4fd1dca8b1ccd078f4531ccbc28
Howard-12 <s0989175085@gmail.com>
2025-06-05 18:51:27 +1000

    fix: template structure, and readme

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ nix flake init -t github:Howard-12/zig-template
 
 Choose your compiler version (modify zig_version in `flake.nix`):
 ```zig
-        # compiler version
-        zig_version = "0.14.1";
-        zig = zig_overlay.packages.${system}.${zig_version};
+# compiler version
+zig_version = "0.14.1";
+zig = zig_overlay.packages.${system}.${zig_version};
 ```
 
 run `nix develop` to start the nix shell or run `direnv allow`
--- flake.nix
+++ flake.nix
@@ -1,36 +1,10 @@
 {
   description = "flake zig dev";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self }: {
 
-  inputs.zig_overlay.url = "github:mitchellh/zig-overlay";
-  inputs.zig_overlay.inputs.flake-utils.follows = "flake-utils";
-
-  inputs.zls.url = "github:zigtools/zls";
-
-  outputs = input@{ self, nixpkgs, flake-utils, zig_overlay, zls, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { system = system; };
-
-        # compiler version
-        zig_version = "0.14.1";
-        zig = zig_overlay.packages.${system}.${zig_version};
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            zls.packages.${system}.default
-            zig
-          ];
-
-          shellHook = ''
-            echo 'zig' "$(zig version)"
-          '';
-        };
-      }
-    ) // {
-      templates.default = {
-        path = ./.;
-        description = "Zig dev flake template";
-      };
+    templates.default = {
+        path = ./zig;
+        description = "A Zig project template.";
     };
+  };
 }
